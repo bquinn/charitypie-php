84843720a822c621d7dddfc89f0374fd8659c59e
brendanquinnoz <brendanquinnoz@ceb9b938-fc77-11dd-87c9-111d11f6bf44>
2009-02-17 22:38:13 +0000

    Now has a database with some charities and enough code to browse around the charities list. A couple of cosmetic changes too.

--- application/controllers/CharitiesController.php
+++ application/controllers/CharitiesController.php
@@ -4,7 +4,60 @@
   */
 class CharitiesController extends Zend_Controller_Action {
   protected $_model;
+  protected $_charityCategoryModel;
 
   public function indexAction() {
+	$category_model = $this->_getCharityCategoryModel();
+	$charity_categories = $category_model->fetchCategoryList();
+	$this->view->charity_categories = $charity_categories;
   }
+
+  public function viewAction() {
+	$charity_id = $this->_getParam("charityid");
+
+	$charity_model = $this->_getModel();
+	$category_model = $this->_getCharityCategoryModel();
+
+	# populate charity categories dropdown
+	$charity_categories = $category_model->fetchCategoryList();
+	$this->view->charity_categories = $charity_categories;
+
+	# get info on the charity itself
+	$charity = $charity_model->fetchCharityById($charity_id);
+	$this->view->charity = $charity;
+  }
+
+  public function categoryAction() {
+	$category_id = $this->_getParam("categoryid");
+
+	$charity_model = $this->_getModel();
+	$category_model = $this->_getCharityCategoryModel();
+
+	# populate charity categories dropdown
+	$charity_categories = $category_model->fetchCategoryList();
+	$this->view->charity_categories = $charity_categories;
+
+	# get info on this category for heading
+	$category_info = $category_model->fetchById($category_id);
+	$this->view->category_info = $category_info;
+
+	# get info on the charity itself
+	$charities = $charity_model->fetchCharitiesByCategory($category_id);
+	$this->view->charities = $charities;
+  }
+
+  protected function _getModel() {
+    if (null === $this->_model) {
+      $this->_model = new Model_Charity();
+    }
+    return $this->_model;
+  }
+
+  protected function _getCharityCategoryModel() {
+    if (null === $this->_charityCategoryModel) {
+      $this->_charityCategoryModel = new Model_CharityCategory();
+    }
+    return $this->_charityCategoryModel;
+  }
+
 }
