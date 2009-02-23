bf92e897b6eaa6381fd7c1fa905fd88b503de8d3
nathan@yura.net <nathan@yura.net@ceb9b938-fc77-11dd-87c9-111d11f6bf44>
2009-02-23 14:53:15 +0000

    New helper to output site's base url

---
+++ trunk/application/views/helpers/BaseUrl.php
@@ -0,0 +1,19 @@
+<?php
+/** Zend_View_Helper_Abstract.php */
+require_once 'Zend/View/Helper/Abstract.php';
+
+/**
+ * Grab the sites base url
+ *
+ * Author: Nathan Sudell (nathan@yura.net)
+ * Company: MyCharityPie
+ * Date: 2009-02-20
+ * 
+ */
+class Zend_View_Helper_BaseUrl extends Zend_View_Helper_Abstract
+{
+    public function baseurl()
+    {
+        return Zend_Controller_Front::getInstance()->getBaseUrl();
+    }
+}
