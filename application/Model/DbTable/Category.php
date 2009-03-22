a2ef648a85e0a2d6d105dd5ab5d6fc3464c55b48
nathan@yura.net <nathan@yura.net@ceb9b938-fc77-11dd-87c9-111d11f6bf44>
2009-03-22 22:42:42 +0000

    Missed files....

---
+++ application/Model/DbTable/Category.php
@@ -0,0 +1,10 @@
+<?php
+ /**
+  * This is the DbTable class for the category table,
+  * the main table for the site.
+  */
+class Model_DbTable_Category extends Zend_Db_Table_Abstract {
+ /** Table name */
+  protected $_name = 'categories';
+
+}
