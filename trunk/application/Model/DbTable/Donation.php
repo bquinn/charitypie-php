cb565b139ce9a8e74b7e6a6961318c4262f917a7
nathan@yura.net <nathan@yura.net@ceb9b938-fc77-11dd-87c9-111d11f6bf44>
2009-03-23 19:30:49 +0000

    Very basic donate functionality

---
+++ trunk/application/Model/DbTable/Donation.php
@@ -0,0 +1,26 @@
+<?php
+
+class Model_DbTable_Donation extends Zend_Db_Table_Abstract {
+  protected $_name = "donations";
+
+  public function fetchCurrentUserDonation($userId) {
+    $select = new Zend_Db_Table_Select($this);
+    $select->where('user_id = ?',$userId);
+    $donation = $this->fetchRow($select);
+    if ($donation) {
+      $donation = $donation->toArray();
+    } else {
+      $donation = null;
+    }
+    return $donation;
+  }
+
+  public function create($userId,$donation) {
+    $data = array(
+      'user_id'=>$userId,
+      'amount'=>$donation,
+      'date'=>date('Y-m-d h:m:s')
+    );
+    $this->insert($data);
+  }
+}
