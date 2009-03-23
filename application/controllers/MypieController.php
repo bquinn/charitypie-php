0610c8eb683e5b6a7ef11f666df369f13164aa40
nathan@yura.net <nathan@yura.net@ceb9b938-fc77-11dd-87c9-111d11f6bf44>
2009-03-23 20:13:02 +0000

    Moved "My Causes" section to the causes index

--- application/controllers/MypieController.php
+++ application/controllers/MypieController.php
@@ -72,8 +72,6 @@ class MyPieController extends Zend_Controller_Action {
     $pieId = $this->_helper->Pie->getUserPieId();
     $userId = $this->getUserId();
 
-    $causes   = $this->getUserCauses();
-
     $donation = null;
     if ($userId > 0) {
       $donation = $this->_getDonationModel()->fetchCurrentUserDonation($userId);
@@ -86,42 +84,7 @@ class MyPieController extends Zend_Controller_Action {
     $this->view->user       = $userId;
     $this->view->donation   = $donation;
     $this->view->pieId      = $pieId;
-    $this->view->causes     = $causes;
-    $this->view->cause_form = $this->getCreateCauseForm();
     $this->view->messages   = $this->_helper->FlashMessenger->getMessages();
-  }
-
-  public function createcauseAction() {
-    if (!$this->getRequest()->isPost()) {
-      return $this->_forward('index');
-    }
-
-    $form = $this->getCreateCauseForm();
-
-    if (!$form->isValid($_POST)) {
-      $this->view->form = $form;
-      return $this->render('form');
-    }
-
-    $userId = $this->getUserId();
-    $posted = $form->getValues();
-
-    if (!$userId) {
-      return $this->getHelper(redirector)->direct('index');
-    }
-
-    $causeId = $this->_getCauseModel()->create($posted['name'],$userId);
-    $pieId   = $this->_getModel()->createPie($causeId,'CAUSE');
-
-    $this->getHelper(redirector)->direct('index');
-  }
-
-  public function getCreateCauseForm() {
-    $form = new Zend_Form();
-    $form->setAction('mypie/createcause');
-    $form->addElement('text','name',array('label'=>'cause name'));
-    $form->addElement('submit','create');
-    return $form;
   }
 
   public function donateAction() {
